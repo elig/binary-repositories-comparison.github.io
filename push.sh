7bbb6dae5feb2038e27422e32a2b1019a6b3565d
Eli Givoni <elig@jfrog.com>
2015-06-06 19:16:19 +0300

    init

---
+++ push.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+git remote set-url --push origin `git config remote.origin.url | sed -e 's/^git:/https:/'`
+
+
+if ! [ -d build/asciidoc ]; then
+    echo "No new sources, so not syncing"
+    exit 0
+fi
+
+# double check there were git changes
+###################################################################
+git diff-index --quiet HEAD
+dirty=$?
+if [ "$dirty" != "0" ]; then
+git add index.html
+git add images
+git commit -a -m "Changes in table"
+git push
+
+# Create a tag
+tagName=$(date +%s)
+git tag $tagName
+git push origin $tagName
+fi
+
+exit 0
